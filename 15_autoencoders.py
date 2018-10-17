42fe43b77e03e63db072a3e89582c52a18dc1775
ranchirino <ranchirino@gmail.com>
2018-10-17 17:04:47 -0500

    Update 15_autoencoders.py

--- 15_autoencoders.py
+++ 15_autoencoders.py
@@ -106,3 +106,96 @@ plt.ylabel("$z_2$", fontsize=18, rotation=0)
 plt.show()
 
 
+#%% Stacked Autoencoders
+# Let's use MNIST:
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/")
+
+# Train all layers at once
+# Let's build a stacked Autoencoder with 3 hidden layers and 1 output layer (ie. 2 stacked Autoencoders).
+# We will use ELU activation, He initialization and L2 regularization.
+# The code should look very familiar, except that there are no labels (no y):
+
+reset_graph()
+
+from functools import partial
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 150  # codings
+n_hidden3 = n_hidden1
+n_outputs = n_inputs
+
+learning_rate = 0.01
+l2_reg = 0.0001
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+
+he_init = tf.contrib.layers.variance_scaling_initializer() # He initialization
+#Equivalent to:
+#he_init = lambda shape, dtype=tf.float32: tf.truncated_normal(shape, 0., stddev=np.sqrt(2/shape[0]))
+l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
+my_dense_layer = partial(tf.layers.dense,
+                         activation=tf.nn.elu,
+                         kernel_initializer=he_init,
+                         kernel_regularizer=l2_regularizer)
+
+hidden1 = my_dense_layer(X, n_hidden1)
+hidden2 = my_dense_layer(hidden1, n_hidden2)
+hidden3 = my_dense_layer(hidden2, n_hidden3)
+outputs = my_dense_layer(hidden3, n_outputs, activation=None)
+
+reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
+
+reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+loss = tf.add_n([reconstruction_loss] + reg_losses)
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver() # not shown in the book
+
+# Now let's train it! Note that we don't feed target values (y_batch is not used). This is unsupervised training.
+
+n_epochs = 5
+batch_size = 150
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        n_batches = mnist.train.num_examples // batch_size
+        for iteration in range(n_batches):
+            # print("\r{}%".format(100 * iteration // n_batches), end="") # not shown in the book
+            sys.stdout.flush()                                          # not shown
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch})
+        loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})   # not shown
+        print("\r{}".format(epoch), "Train MSE:", loss_train)           # not shown
+        saver.save(sess, "./my_model_all_layers.ckpt")                  # not shown
+
+# 0 Train MSE: 0.020855438
+# 1 Train MSE: 0.011372581
+# 2 Train MSE: 0.010224564
+# 3 Train MSE: 0.009900457
+# 4 Train MSE: 0.010375758
+
+# This function loads the model, evaluates it on the test set (it measures the reconstruction error),
+# then it displays the original image and its reconstruction:
+def show_reconstructed_digits(X, outputs, model_path = None, n_test_digits = 2):
+    with tf.Session() as sess:
+        if model_path:
+            saver.restore(sess, model_path)
+        X_test = mnist.test.images[:n_test_digits]
+        outputs_val = outputs.eval(feed_dict={X: X_test})
+
+    fig = plt.figure(figsize=(8, 3 * n_test_digits))
+    for digit_index in range(n_test_digits):
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 1)
+        plot_image(X_test[digit_index])
+        plt.subplot(n_test_digits, 2, digit_index * 2 + 2)
+        plot_image(outputs_val[digit_index])
+
+show_reconstructed_digits(X, outputs, "./my_model_all_layers.ckpt")
+
+
