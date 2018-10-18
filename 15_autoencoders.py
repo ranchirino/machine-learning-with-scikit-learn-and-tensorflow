deb46efbc2047d7af2796ca400957f749379d6f0
ranchirino <ranchirino@gmail.com>
2018-10-18 08:03:00 -0500

    Update 15_autoencoders.py

--- 15_autoencoders.py
+++ 15_autoencoders.py
@@ -199,3 +199,78 @@ def show_reconstructed_digits(X, outputs, model_path = None, n_test_digits = 2):
 show_reconstructed_digits(X, outputs, "./my_model_all_layers.ckpt")
 
 
+#%% Tying Weights
+# It is common to tie the weights of the encoder and the decoder (weights_decoder = tf.transpose(weights_encoder)).
+# Unfortunately this makes it impossible (or very tricky) to use the tf.layers.dense() function,
+# so we need to build the Autoencoder manually:
+
+reset_graph()
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 150  # codings
+n_hidden3 = n_hidden1
+n_outputs = n_inputs
+
+learning_rate = 0.01
+l2_reg = 0.0005
+
+activation = tf.nn.elu
+regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+
+weights1_init = initializer([n_inputs, n_hidden1])
+weights2_init = initializer([n_hidden1, n_hidden2])
+
+weights1 = tf.Variable(weights1_init, dtype=tf.float32, name="weights1")
+weights2 = tf.Variable(weights2_init, dtype=tf.float32, name="weights2")
+weights3 = tf.transpose(weights2, name="weights3")  # tied weights
+weights4 = tf.transpose(weights1, name="weights4")  # tied weights
+
+biases1 = tf.Variable(tf.zeros(n_hidden1), name="biases1")
+biases2 = tf.Variable(tf.zeros(n_hidden2), name="biases2")
+biases3 = tf.Variable(tf.zeros(n_hidden3), name="biases3")
+biases4 = tf.Variable(tf.zeros(n_outputs), name="biases4")
+
+hidden1 = activation(tf.matmul(X, weights1) + biases1)
+hidden2 = activation(tf.matmul(hidden1, weights2) + biases2)
+hidden3 = activation(tf.matmul(hidden2, weights3) + biases3)
+outputs = tf.matmul(hidden3, weights4) + biases4
+
+reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
+reg_loss = regularizer(weights1) + regularizer(weights2)
+loss = reconstruction_loss + reg_loss
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+
+n_epochs = 5
+batch_size = 150
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        n_batches = mnist.train.num_examples // batch_size
+        for iteration in range(n_batches):
+            # print("\r{}%".format(100 * iteration // n_batches), end="")
+            sys.stdout.flush()
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch})
+        loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})
+        print("\r{}".format(epoch), "Train MSE:", loss_train)
+        saver.save(sess, "./my_model_tying_weights.ckpt")
+
+# 0 Train MSE: 0.015602261
+# 1 Train MSE: 0.016729439
+# 2 Train MSE: 0.015856257
+# 3 Train MSE: 0.017920662
+# 4 Train MSE: 0.0178301
+
+show_reconstructed_digits(X, outputs, "./my_model_tying_weights.ckpt")
+
