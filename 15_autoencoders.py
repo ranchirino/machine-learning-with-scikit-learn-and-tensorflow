1c6956b68c9da87dfff5680db049c490385636b1
ranchirino <ranchirino@gmail.com>
2018-10-18 08:27:11 -0500

    Update 15_autoencoders.py

--- 15_autoencoders.py
+++ 15_autoencoders.py
@@ -274,3 +274,102 @@ with tf.Session() as sess:
 
 show_reconstructed_digits(X, outputs, "./my_model_tying_weights.ckpt")
 
+# there are a few important things to note:
+# • First, weight3 and weights4 are not variables, they are respectively the transpose
+# of weights2 and weights1 (they are “tied” to them).
+# • Second, since they are not variables, it’s no use regularizing them: we only regularize
+# weights1 and weights2.
+# • Third, biases are never tied, and never regularized.
+
+
+#%% Training one Autoencoder at a time in multiple graphs
+# There are many ways to train one Autoencoder at a time.
+# The first approach is to train each Autoencoder using a different graph,
+# then we create the Stacked Autoencoder by simply initializing it with the weights and biases copied from these Autoencoders.
+# Let's create a function that will train one autoencoder and return the transformed training set
+# (i.e., the output of the hidden layer) and the model parameters.
+
+reset_graph()
+
+from functools import partial
+
+def train_autoencoder(X_train, n_neurons, n_epochs, batch_size,
+                      learning_rate=0.01, l2_reg=0.0005, seed=42,
+                      hidden_activation=tf.nn.elu,
+                      output_activation=tf.nn.elu):
+    graph = tf.Graph()
+    with graph.as_default():
+        tf.set_random_seed(seed)
+
+        n_inputs = X_train.shape[1]
+
+        X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+
+        my_dense_layer = partial(
+            tf.layers.dense,
+            kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+            kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg))
+
+        hidden = my_dense_layer(X, n_neurons, activation=hidden_activation, name="hidden")
+        outputs = my_dense_layer(hidden, n_inputs, activation=output_activation, name="outputs")
+
+        reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
+
+        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        loss = tf.add_n([reconstruction_loss] + reg_losses)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        training_op = optimizer.minimize(loss)
+
+        init = tf.global_variables_initializer()
+
+    with tf.Session(graph=graph) as sess:
+        init.run()
+        for epoch in range(n_epochs):
+            n_batches = len(X_train) // batch_size
+            for iteration in range(n_batches):
+                # print("\r{}%".format(100 * iteration // n_batches), end="")
+                sys.stdout.flush()
+                indices = rnd.permutation(len(X_train))[:batch_size]
+                X_batch = X_train[indices]
+                sess.run(training_op, feed_dict={X: X_batch})
+            loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})
+            print("\r{}".format(epoch), "Train MSE:", loss_train)
+        params = dict([(var.name, var.eval()) for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)])
+        hidden_val = hidden.eval(feed_dict={X: X_train})
+        return hidden_val, params["hidden/kernel:0"], params["hidden/bias:0"], params["outputs/kernel:0"], params[
+            "outputs/bias:0"]
+
+# Now let's train two Autoencoders. The first one is trained on the training data,
+# and the second is trained on the previous Autoencoder's hidden layer output:
+
+hidden_output, W1, b1, W4, b4 = train_autoencoder(mnist.train.images, n_neurons=300, n_epochs=4, batch_size=150,
+                                                  output_activation=None)
+_, W2, b2, W3, b3 = train_autoencoder(hidden_output, n_neurons=150, n_epochs=4, batch_size=150)
+
+# 0 Train MSE: 0.01839451
+# 1 Train MSE: 0.018561972
+# 2 Train MSE: 0.019172184
+# 3 Train MSE: 0.019527696
+# 0 Train MSE: 0.004414982
+# 1 Train MSE: 0.004626561
+# 2 Train MSE: 0.004617909
+# 3 Train MSE: 0.004764604
+
+# Finally, we can create a Stacked Autoencoder
+# by simply reusing the weights and biases from the Autoencoders we just trained:
+
+reset_graph()
+
+n_inputs = 28*28
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+hidden1 = tf.nn.elu(tf.matmul(X, W1) + b1)
+hidden2 = tf.nn.elu(tf.matmul(hidden1, W2) + b2)
+hidden3 = tf.nn.elu(tf.matmul(hidden2, W3) + b3)
+outputs = tf.matmul(hidden3, W4) + b4
+
+show_reconstructed_digits(X, outputs)
+
+
+# Training one Autoencoder at a time in a single graph
